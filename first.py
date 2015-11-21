a1bb95a5741594071b0b5f91a7f3a366ce129c84
ForseFox <Julidonetsk@yandex.ua>
2015-11-21 15:53:21 +0200

    My Django Girls app, first commit

---
+++ first.py
@@ -0,0 +1,62 @@
+# print('Hello, Django girls!')
+# a = 2
+# b = 3
+# c = a * b
+# print(c)
+# if 5 < 3:
+#    print('really!')
+# else:
+#    print('false')
+#
+# name = 'Masha'
+# if name == 'Jul':
+#    print('Hey, Jul!')
+# elif name == 'Jane':
+#    print('Hello, Jane!')
+# else:
+#    print('Hey, you!')
+#
+# volume = 37
+# if volume <10:
+#    print('tiho')
+# elif 10 <= volume < 30:
+#    print('norm')
+# elif 30 <= volume < 50:
+#    print('gromko')
+# else:
+#    print('My ears are hurting')
+#
+# def sum():
+#     a = 5
+#     b = 7
+#     c = a + b
+#     print(c)
+# sum()
+# name = "345"
+
+
+# name = 345
+#
+# def name1():
+#     if name == 'Sasha':
+#         print('Hey, Sasha!')
+#     elif name == 'Olia':
+#         print('Hello, Olia!')
+#     else:
+#         print('Hello, anonymous!')
+#
+# name1()
+# print(name)
+
+
+# def hi(name):
+#     print ('Hi '+ name + '!')
+#
+# girls = ['Jul', 'Vika', 'Olia', 'Sasha']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
+
+
+for i in range(1, 6):
+    print(i)
